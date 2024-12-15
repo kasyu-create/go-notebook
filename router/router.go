7aa20ad58df9a6b2371f9f0587304157a8bcb5da
Haruki Higashino <ntthigashino@icloud.com>
2024-12-15 16:24:37 +0900

    feat:ログインログアウトが出来るように（途中）

--- router/router.go
+++ router/router.go
@@ -10,7 +10,12 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func NewRouter(uc controller.IUserController, tc controller.ITaskController, gc controller.IGenreController) *echo.Echo {
+func NewRouter(
+	uc controller.IUserController,
+	tc controller.ITaskController,
+	gc controller.IGenreController,
+	ac controller.IAuthController, // AuthControllerを追加
+) *echo.Echo {
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"http://localhost:3000", os.Getenv("FE_URL")},
@@ -19,19 +24,21 @@ func NewRouter(uc controller.IUserController, tc controller.ITaskController, gc 
 		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE"},
 		AllowCredentials: true,
 	}))
+
 	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
 		CookiePath:     "/",
 		CookieDomain:   os.Getenv("API_DOMAIN"),
 		CookieHTTPOnly: true,
 		CookieSameSite: http.SameSiteNoneMode,
-		//CookieSameSite: http.SameSiteDefaultMode,
-		//CookieMaxAge:   60,
 	}))
+
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.LogIn)
 	e.POST("/logout", uc.LogOut)
 	e.GET("/csrf", uc.CsrfToken)
 	e.GET("/genres", gc.GetAllGenres)
+
+	// JWT認証が必要なルート
 	t := e.Group("/tasks")
 	t.Use(echojwt.WithConfig(echojwt.Config{
 		SigningKey:  []byte(os.Getenv("SECRET")),
@@ -42,5 +49,12 @@ func NewRouter(uc controller.IUserController, tc controller.ITaskController, gc 
 	t.POST("", tc.CreateTask)
 	t.PUT("/:taskId", tc.UpdateTask)
 	t.DELETE("/:taskId", tc.DeleteTask)
+
+	// 認証状態確認用エンドポイント
+	e.GET("/me", ac.Me, echojwt.WithConfig(echojwt.Config{
+		SigningKey:  []byte(os.Getenv("SECRET")),
+		TokenLookup: "cookie:token", // クッキーからトークンを取得
+	}))
+
 	return e
 }
